481280829d7322e26b9d03f47b5d65c173fb8f62
noah <noah.prezant@protonmail.ch>
2022-09-23 15:49:06 +0000

    Add dotnet6 example project

---
+++ Examples/Hello-Dotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+using MPI;
+
+Console.WriteLine("Hello world");
+
+MPI.Environment.Run(ref args, communicator =>
+{
+    Console.WriteLine("Hello, from process number "
+                             + communicator.Rank + " of "
+                             + communicator.Size);
+});
+Console.WriteLine("done");
